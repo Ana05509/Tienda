be7dd55cfca43d2113822b19bdf1c2ce1d606c4e
Ana05509 <ana.salguero3883@utc.edu.ec>
2024-12-17 08:39:03 -0500

    SUBIR

--- sistema_ventas/ventas/migrations/__init__.py
+++ sistema_ventas/ventas/migrations/__init__.py
@@ -1,6 +1,6 @@
 # ventas/__init__.py
 
 # Importa los viewsets para que se puedan acceder fácilmente desde el paquete
-from .views import ProductoViewSet, ClienteViewSet, VentaViewSet
+from .views import ProductoViewSet, ClienteViewSet, VentaViewSet # type: ignore
 
 # Puedes inicializar algunas configuraciones o variables aquí si es necesario
